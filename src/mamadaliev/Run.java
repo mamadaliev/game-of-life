935ad37ce59e6e9e2dac248bc5dc74bca67b3606
mamadaliev <mamadaliev@yahoo.com>
2019-03-24 20:22:51 +0300

    fix(mamadaliev/): rename class name

--- src/mamadaliev/Run.java
+++ src/mamadaliev/Run.java
@@ -1,6 +1,6 @@
 package mamadaliev;
 
-public class Main {
+public class Run {
     public static void main(String[] args) {
         Game game = new Game(1280,720, 1280, 720, 5);
         game.init();
