50c593a4a0bf84a85375d77689f8e01a7dae0574
mamadaliev <mamadaliev@yahoo.com>
2019-04-06 12:13:34 +0300

    add(mamadaliev/): Configuration interface

---
+++ src/mamadaliev/Config.java
@@ -0,0 +1,10 @@
+package mamadaliev;
+
+public interface Config {
+    int DEFAULT_WINDOW_WIDTH    = 1280;
+    int DEFAULT_WINDOW_HEIGHT   = 720;
+    int DEFAULT_MATRIX_WIDTH    = 1000;
+    int DEFAULT_MATRIX_HEIGHT   = 500;
+    int DEFAULT_PERCENT         = 80;
+    int DEFAULT_RADIUS          = 1;
+}
